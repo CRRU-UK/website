633506cfb275beab7bf4828745ba0c0204fa8e81
Sam K <samuel.kindler@icloud.com>
2024-07-04 13:26:18 +0100

    Add Ethics Statement page (#28)

---
+++ src/pages/research/ethics.tsx
@@ -0,0 +1,38 @@
+import type { NextPage, GetServerSideProps } from 'next';
+
+import type { PageData } from '@/helpers/types';
+
+import sitemap from '@/data/sitemap.json';
+
+import getPageContent from '@/helpers/getPageContent';
+
+import CommonPage from '@/layout/CommonPage';
+
+interface PageProps {
+  data: PageData,
+}
+
+const Page: NextPage<PageProps> = ({
+  data,
+}) => (
+  <CommonPage
+    page={sitemap['ethics-statement']}
+    parent={sitemap.research}
+    breadcrumbs={[sitemap.research, sitemap['ethics-statement']]}
+    data={data}
+  />
+);
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async (ctx) => {
+  const preview = ctx?.query.preview === 'true';
+  const data = await getPageContent(sitemap['ethics-statement'].path, { preview });
+
+  return {
+    props: {
+      preview,
+      data,
+    },
+  };
+};
+
+export default Page;
