30ea6905cfccf5722a75f2806ad56e1ff9d2fbf2
samwisekind <samuel.kindler@icloud.com>
2024-10-27 16:34:57 +0000

    Updates

--- src/components/Toolbar/Toolbar.spec.tsx
+++ src/components/Toolbar/Toolbar.spec.tsx
@@ -15,6 +15,30 @@ it('Passes accessibility with default props', async () => act(async () => {
     <Toolbar
       // @ts-expect-error String of enum value
       type="bottlenose-dolphin"
+      previous={null}
+      next={null}
+    />,
+  );
+
+  const results = await axe(container);
+
+  expect(results).toHaveNoViolations();
+}));
+
+it('Passes accessibility with optional props', async () => act(async () => {
+  const mockedData = {
+    id: 'mocked-id',
+    reference: 'mocked-reference',
+    name: 'mocked-name',
+    slug: 'mocked slug',
+  };
+
+  const { container } = render(
+    <Toolbar
+      // @ts-expect-error String of enum value
+      type="bottlenose-dolphin"
+      previous={mockedData}
+      next={mockedData}
     />,
   );
 
