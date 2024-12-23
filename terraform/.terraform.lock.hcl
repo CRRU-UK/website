f0517ead762ac1bea1bb39e26315cb631c127431
samwisekind <samuel.kindler@icloud.com>
2024-12-23 19:52:02 +0000

    Migrate website Terraform

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,49 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/cloudflare/cloudflare" {
+  version     = "4.48.0"
+  constraints = "~> 4.48"
+  hashes = [
+    "h1:SX42e3k73IcFcrQlZ2e/Veqt2tvCMy6fwlo5yNUktCE=",
+    "zh:04c0a49c2b23140b2f21cfd0d52f9798d70d3bdae3831613e156aabe519bbc6c",
+    "zh:185f21b4834ba63e8df1f84aa34639d8a7e126429a4007bb5f9ad82f2602a997",
+    "zh:234724f52cb4c0c3f7313d3b2697caef26d921d134f26ae14801e7afac522f7b",
+    "zh:38a56fcd1b3e40706af995611c977816543b53f1e55fe2720944aae2b6828fcb",
+    "zh:419938f5430fc78eff933470aefbf94a460a478f867cf7761a3dea177b4eb153",
+    "zh:4b46d92bfde1deab7de7ba1a6bbf4ba7c711e4fd925341ddf09d4cc28dae03d8",
+    "zh:537acd4a31c752f1bae305ba7190f60b71ad1a459f22d464f3f914336c9e919f",
+    "zh:5ff36b005aad07697dd0b30d4f0c35dbcdc30dc52b41722552060792fa87ce04",
+    "zh:635c5ee419daea098060f794d9d7d999275301181e49562c4e4c08f043076937",
+    "zh:859277c330d61f91abe9e799389467ca11b77131bf34bedbef52f8da68b2bb49",
+    "zh:890df766e9b839623b1f0437355032a3c006226a6c200cd911e15ee1a9014e9f",
+    "zh:927dfdb8d9aef37ead03fceaa29e87ba076a3dd24e19b6cefdbb0efe9987ff8c",
+    "zh:bbf2226f07f6b1e721877328e69ded4b64f9c196634d2e2429e3cfabbe41e532",
+    "zh:daeed873d6f38604232b46ee4a5830c85d195b967f8dbcafe2fcffa98daf9c5f",
+    "zh:f8f2fc4646c1ba44085612fa7f4dbb7cbcead43b4e661f2b98ddfb4f68afc758",
+  ]
+}
+
+provider "registry.terraform.io/digitalocean/digitalocean" {
+  version     = "2.46.1"
+  constraints = "~> 2.46"
+  hashes = [
+    "h1:RLRn2WL8Hwb08G1QHEkfo+6+dCZv2/LUiQblZmPy7yo=",
+    "zh:3526081f8a54f40cc15deedc451e1fd6e816af1d64910e5db5c4feb344efb2aa",
+    "zh:448140c8a9cbc3c0f98cafc77eeefb67216f8a508ba2d7519a95a1f35b985f14",
+    "zh:4a751404e28d81fd3665b7d3771e3e56ffa577f8da5568da6c81d486bf8debe1",
+    "zh:5106ed89005f49e7125ab6939a78a05c36db4a55e0d8ac0c77c0a8df8f36e054",
+    "zh:53f455e0ab52a2f99d42ca3e8154153be387ae375d616f61fea9aca94529a03f",
+    "zh:7360d2c1aa2840de001803f0d553898fc0278b6c86cd32bf74118a336dc810d7",
+    "zh:83a7a62c60ccac1cd88827c679175ab7197941820bacb0c1dd109519ac8389d4",
+    "zh:9771d84362d27e11e16002841ab5375f8f66984de84cc975a2d0650c42b7f81f",
+    "zh:a7271363d571aab0056886b00fdf336cc8d5ab780c56ce2abd4a409f3a77b25b",
+    "zh:b2067cdac25b2f21d5b13f381a1178a7a4cb164e24d248c22216738ae84d0a67",
+    "zh:b3189cc0989e858f8473f18a57f0f69cd99916ec5e55eba5031159f8fe6f7f56",
+    "zh:c0324136a3fbff814d82660b21225fd45bb7d04350cf669cc8651e9ae4fb467f",
+    "zh:c6f4be572b3d008cb67e31f5588ff2d1a3807e2e4dc880bfec08e217d2fa09df",
+    "zh:ddeee2f45a510ecb0ef0cfe5981e7f4f8286baab171b13ac87c46f269ced2dab",
+    "zh:f13d58ec144f19d6520df799c9915cec4b911dd7cb347732645504818b2b9f20",
+    "zh:f984d88e4fccf533adf0b73f32c23aca503596b6872fde5f08dcb76134130175",
+  ]
+}
---
+++ terraform/website.tf
@@ -0,0 +1,201 @@
+locals {
+  app_name          = "app"
+  digitalocean_host = replace(digitalocean_app.deployment.default_ingress, "/(http(s)?://)/", "")
+}
+
+resource "digitalocean_app" "website_app" {
+  spec {
+    name   = "website"
+    region = "lon"
+
+    alert {
+      rule     = "DEPLOYMENT_FAILED"
+      disabled = false
+    }
+
+    alert {
+      rule     = "DEPLOYMENT_LIVE"
+      disabled = false
+    }
+
+    domain {
+      name = var.website_domain
+    }
+
+    domain {
+      name = "www.${var.website_domain}"
+    }
+
+    ingress {
+      rule {
+        component {
+          name                 = local.app_name
+          preserve_path_prefix = false
+        }
+
+        match {
+          path {
+            prefix = "/"
+          }
+        }
+      }
+    }
+
+    service {
+      name = local.app_name
+
+      dockerfile_path = "Dockerfile"
+
+      instance_size_slug = "apps-s-1vcpu-0.5gb"
+      instance_count     = 1
+
+      http_port      = 3000
+      internal_ports = []
+
+      github {
+        repo           = "CRRU-UK/website"
+        branch         = "main"
+        deploy_on_push = true
+      }
+
+      health_check {
+        http_path             = "/api/status"
+        initial_delay_seconds = 0
+        period_seconds        = 10
+        timeout_seconds       = 1
+        success_threshold     = 1
+        failure_threshold     = 9
+      }
+
+      alert {
+        rule     = "RESTART_COUNT"
+        value    = "2"
+        operator = "GREATER_THAN"
+        window   = "FIVE_MINUTES"
+      }
+
+      env {
+        key   = "NODE_ENV"
+        value = "production"
+        scope = "RUN_AND_BUILD_TIME"
+        type  = "GENERAL"
+      }
+
+      env {
+        key   = "NODE_CONTENTFUL_SPACE_ID"
+        value = var.contentful_space_id
+        scope = "RUN_TIME"
+        type  = "GENERAL"
+      }
+
+      env {
+        key   = "NODE_CONTENTFUL_ENVIRONMENT"
+        value = var.contentful_environment
+        scope = "RUN_TIME"
+        type  = "GENERAL"
+      }
+
+      env {
+        key   = "NODE_CONTENTFUL_DELIVERY_API_TOKEN"
+        value = var.contentful_delivery_api_token
+        scope = "RUN_TIME"
+        type  = "SECRET"
+      }
+
+      env {
+        key   = "NODE_CONTENTFUL_PREVIEW_API_TOKEN"
+        value = var.contentful_preview_api_token
+        scope = "RUN_TIME"
+        type  = "SECRET"
+      }
+
+      env {
+        key   = "NEXT_PUBLIC_CLOUDFLARE_CHALLENGE_SITE_KEY"
+        value = cloudflare_turnstile_widget.website_challenge.id
+        scope = "BUILD_TIME"
+        type  = "GENERAL"
+      }
+
+      env {
+        key   = "NODE_CLOUDFLARE_CHALLENGE_SECRET_KEY"
+        value = cloudflare_turnstile_widget.website_challenge.secret
+        scope = "RUN_TIME"
+        type  = "SECRET"
+      }
+
+      env {
+        key   = "NEXT_PUBLIC_GOOGLE_ANALYTICS_ID"
+        value = var.google_analytics_id
+        scope = "BUILD_TIME"
+        type  = "GENERAL"
+      }
+
+      env {
+        key   = "NODE_SMTP_HOST"
+        value = var.smtp_host
+        scope = "RUN_TIME"
+        type  = "SECRET"
+      }
+
+      env {
+        key   = "NODE_SMTP_PORT"
+        value = var.smtp_port
+        scope = "RUN_TIME"
+        type  = "SECRET"
+      }
+
+      env {
+        key   = "NODE_SMTP_USERNAME"
+        value = var.smtp_username
+        scope = "RUN_TIME"
+        type  = "SECRET"
+      }
+
+      env {
+        key   = "NODE_SMTP_PASSWORD"
+        value = var.smtp_password
+        scope = "RUN_TIME"
+        type  = "SECRET"
+      }
+
+      env {
+        key   = "NODE_SIGHTING_EMAIL"
+        value = var.sighting_email
+        scope = "RUN_TIME"
+        type  = "GENERAL"
+      }
+    }
+  }
+}
+
+resource "cloudflare_turnstile_widget" "website_challenge" {
+  account_id = var.cloudflare_account_id
+
+  domains        = [var.website_domain]
+  name           = "CRRU Website"
+  mode           = "managed"
+  region         = "world"
+  bot_fight_mode = false
+}
+
+resource "cloudflare_record" "website_dns_apex" {
+  zone_id = var.cloudflare_zone_id
+
+  name    = "@"
+  type    = "CNAME"
+  content = local.digitalocean_host
+  ttl     = 1
+  proxied = true
+  comment = "Website (apex)"
+}
+
+resource "cloudflare_record" "website_dns_www" {
+  zone_id = var.cloudflare_zone_id
+
+  name    = "www"
+  type    = "CNAME"
+  content = local.digitalocean_host
+  ttl     = 1
+  proxied = true
+  comment = "Website (www)"
+}
