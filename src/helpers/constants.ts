4414290938545e5850962146f3131df060436311
Sam K <samuel.kindler@icloud.com>
2025-06-19 18:02:13 +0100

    Update default SEO image (#141)
    Signed-off-by: Sam K <samuel.kindler@icloud.com>

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -25,7 +25,7 @@ export const DEFAULT_SEO_OPTIONS = {
 
 export const DEFAULT_SEO_IMAGE = [
   {
-    url: "https://images.ctfassets.net/xe1w9sqtia79/6zrbOLcLyjChTaNt2DpO4S/761c5daf4bc9a577ceaeaf0c33a05072/Default_SEO_Image.jpg",
+    url: "https://images.ctfassets.net/xe1w9sqtia79/6zrbOLcLyjChTaNt2DpO4S/1e8abcbedf3c9580039183bd5cbf910e/default-seo-image.png",
     width: 1200,
     height: 630,
   },
