dec948c8c27243320c96f7a297ca607c80bcd819
samwisekind <samuel.kindler@icloud.com>
2025-06-24 19:40:47 +0100

    Fix `cloudflare_ruleset`

--- terraform/website.tf
+++ terraform/website.tf
@@ -225,6 +225,8 @@ resource "cloudflare_ruleset" "website_image_cache_enable" {
     action      = "set_cache_settings"
     description = "Enable cache for all requests"
 
+    expression = "(http.request.uri.path eq \"/_next/image\")"
+
     action_parameters = {
       cache = true
     }
